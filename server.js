2f61d23787aaea1354a1273fb219e995bb4c90d2
ethanvannest <ethan3vannest@gmail.com>
2022-08-23 17:35:10 -0600

    set up mongoose

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 3000
+const mongoURI = 'mongodb://127.0.0.1:27017/'
+const db = mongoose.connection 
 
+mongoose.connect(mongoURI)
+
+mongoose.connect(mongoURI, () => {
+    console.log('Mongoose is running')
+})
 
 app.get('/', (req,res) => {
     res.send ('this works')
 })
+
+app.get('/res')
 
 app.listen (PORT, () => {
     console.log(`app is running on port, ${PORT}`)
